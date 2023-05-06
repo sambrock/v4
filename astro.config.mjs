1c7765f37a8e9ea7237e215d7ba71eae4b33e88f
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2023-05-06 17:26:12 +0100

    added analytics, updated project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
