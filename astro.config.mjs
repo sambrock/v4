a0d5e9688076dbc5995bd0c917998999355c7062
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2023-05-16 20:06:11 +0100

    fixed?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: 'static',
-  adapter: vercel({
-    analytics: true,
-  }),
-  vite: {
-    define: {
-      'import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-    },
-  },
 });
